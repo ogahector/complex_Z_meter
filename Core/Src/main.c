2960e4e9927928645e188d4d7cf57f453f38127a
ogahector <ogahector@gmail.com>
2025-02-27 16:22:51 +0000

    should be the correct commit now

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -571,7 +571,7 @@ static void MX_TIM6_Init(void)
   htim6.Instance = TIM6;
   htim6.Init.Prescaler = 0;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 44000;
+  htim6.Init.Period = 44;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
